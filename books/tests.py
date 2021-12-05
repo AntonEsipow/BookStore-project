93fa6e944d9598e9c96ccb1448ee9ba348cd79a5
AntonEsipow <esipow.anton99@gmail.com>
2021-12-05 15:18:52 +0300

    Добавлена модель отзывов

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
     """Тест книг"""
 
     def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
+
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='40.00',
+        )
+
+        self.review = Review.objects.create(
+            book=self.book,
+            review='Great review',
+            author=self.user,
         )
 
     def test_book_listing(self):
@@ -33,4 +47,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'Great review')
         self.assertTemplateUsed(response, 'books/book_detail.html')
